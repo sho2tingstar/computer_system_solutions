c0450a4b25647caed63940e6ff6d640ea80c142e
ho2n <jungho2n@gmail.com>
2024-09-10 20:38:38 +0900

    조건부 동작에 관한 내용

--- chapter3/main.c
+++ chapter3/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+long cread(long *xp) {
+	return (xp ? *xp : 0);
+}
+/*
 void decode1(long *xp, long *yp, long *zp) {
 	long temp = *yp;
 	*yp = *xp;
@@ -11,5 +15,10 @@ void decode1(long *xp, long *yp, long *zp) {
 short scale3(short x, short y, short z) {
 	short t = x + 11y + 2z;
 	return t;
-}
+} */
 
+int main() {
+	long *p = 0;
+	cread(p);
+	return 0;
+}
